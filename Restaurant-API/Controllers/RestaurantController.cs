9585d7cedd236e1357f1e66112198d0caae37738
Mateusz Madej <madej256@gmail.com>
2022-06-05 16:22:22 +0200

    End of the CRUD
    - Update request
    - Delete request
    - simple validation
    - some refactoring

--- Restaurant-API/Controllers/RestaurantController.cs
+++ Restaurant-API/Controllers/RestaurantController.cs
@@ -3,28 +3,25 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Restaurant_API.Entities;
 using Restaurant_API.Models;
+using Restaurant_API.Services;
 
 namespace Restaurant_API.Controllers;
 
 [Route("api/restaurant")]
 public class RestaurantController : ControllerBase
 {
-    private readonly RestaurantDbContext _dbContext;
-    private readonly IMapper _mapper;
+    private readonly IRestaurantService _restaurantService;
 
-    public RestaurantController(RestaurantDbContext dbContext, IMapper mapper)
+    public RestaurantController(IRestaurantService restaurantService)
     {
-        _dbContext = dbContext;
-        _mapper = mapper;
+        _restaurantService = restaurantService;
     }
 
     //GET All Restaurant
     [HttpGet]
     public ActionResult<IEnumerable<RestaurantDto>> GetAll()
     {
-        var restaurants = _dbContext.Restaurants.Include(r => r.Address).Include(r => r.Dishes).ToList();
-
-        var restaurantsDtos = _mapper.Map<List<RestaurantDto>>(restaurants);
+        var restaurantsDtos = _restaurantService.GetAll();
         return Ok(restaurantsDtos);
     }
 
@@ -32,23 +29,60 @@ public class RestaurantController : ControllerBase
     [HttpGet("{id}")]
     public ActionResult<RestaurantDto> Get([FromRoute] int id)
     {
-        var restaurant = _dbContext.Restaurants.Include(r => r.Address).Include(r => r.Dishes).FirstOrDefault(r => r.Id == id);
+        var restaurant = _restaurantService.GerById(id);
 
-        if (restaurant == null) return NotFound();
+        if (restaurant is null)
+        {
+            return NotFound();
+        }
 
-        var restaurantDto = _mapper.Map<RestaurantDto>(restaurant);
-        return Ok(restaurantDto);
+        return Ok(restaurant);
     }
 
     //POST Restaurant
     [HttpPost]
     public ActionResult CreateRestaurant([FromBody] CreateRestaurantDto dto)
     {
-        var restaurant = _mapper.Map<Restaurant>(dto);
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        var id = _restaurantService.Create(dto);
 
-        _dbContext.Restaurants.Add(restaurant);
-        _dbContext.SaveChanges();
 
-        return Created($"/api/restaurant/{restaurant.Id}", null);
+        return Created($"/api/restaurant/{id}", null);
+    }
+
+    //DELETE Restaurant
+    [HttpDelete("{id}")]
+    public ActionResult Delete([FromRoute] int id)
+    {
+        var isDeleted = _restaurantService.Delete(id);
+
+        if (!isDeleted)
+        {
+            return NotFound();
+        }
+
+        return NoContent();
+    }
+
+    //PUT Restaurant
+    [HttpPut("{id}")]
+    public ActionResult Update([FromBody]UpdateRestaurantDto dto, [FromRoute] int id)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var isUpdated = _restaurantService.Update(id, dto);
+
+        if (!isUpdated)
+        {
+            return NotFound();
+        }
+
+        return Ok();
     }
 }
--- Restaurant-API/Program.cs
+++ Restaurant-API/Program.cs
@@ -1,6 +1,7 @@
 using System.Reflection;
 using Restaurant_API.Entities;
 using Restaurant_API.Seeders;
+using Restaurant_API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<RestaurantDbContext>();
 builder.Services.AddScoped<RestaurantSeeder>();
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+builder.Services.AddScoped<IRestaurantService, RestaurantService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
